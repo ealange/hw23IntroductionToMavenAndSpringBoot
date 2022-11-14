31a57a60fa3dfd8f6c68444719896103a212492f
Eugen Lange <lange.e@mail.ru>
2022-11-14 22:37:44 +0500

    first commit

---
+++ src/main/java/pro/sky/jd10/lange/hw23introductiontomavenandspringboot/Hw23IntroductionToMavenAndSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.jd10.lange.hw23introductiontomavenandspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw23IntroductionToMavenAndSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw23IntroductionToMavenAndSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/jd10/lange/hw23introductiontomavenandspringboot/Hw23IntroductionToMavenAndSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.jd10.lange.hw23introductiontomavenandspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw23IntroductionToMavenAndSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
